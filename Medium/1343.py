38ef2f3737963befd6f8f1d1d8ac57293b695403
Nathan Schultz <nschult@uwo.ca>
2026-01-08 15:31:00 -0500

    leetcode 1343

--- Medium/1343.py
+++ Medium/1343.py
@@ -1,25 +1,32 @@
+# leetcode 1343
+# medium
+# array, sliding window
+# completed at an unknown time
+
+# i don't remember even doing this question, but here's my solution for it
+# it was not quite working when i found the file for it but i've fixed it up and it works now
 def numOfSubarrays(arr, k, threshold):
 
-    returnVal = 0
     threshold = threshold * k
     subArr = sum(arr[0:k])
+    returnVal = int(subArr >= threshold)
 
-    for i in range(0, len(arr) - k + 1):
-        subArr = arr[i + k - 1] - arr[i - 1]
+    for i in range(1, len(arr) - k + 1):
+        subArr = subArr + arr[i + k - 1] - arr[i - 1]
         returnVal += int(subArr >= threshold)
 
     return returnVal
 
 def main():
 
-    # arr = [2,2,2,2,5,5,5,8]
-    # k = 3
-    # threshold = 4
-    # expected output is 3
-
-    arr = [11,13,17,23,29,31,7,5,2,3]
+    arr = [2,2,2,2,5,5,5,8]
     k = 3
-    threshold = 5
+    threshold = 4
+    #expected output is 3
+
+    # arr = [11,13,17,23,29,31,7,5,2,3]
+    # k = 3
+    # threshold = 5
     # expected ouput is 6
 
     print(numOfSubarrays(arr, k, threshold))
